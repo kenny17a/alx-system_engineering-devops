39f6b88895d1e82bc4f3264161b388c83925d6dd
ibrahim kehinde <91750163+kenny17a@users.noreply.github.com>
2022-09-18 18:37:24 +0100

    Updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# kill a process using pkill with puppet
-# execute pkill command to kill bashscript file killmenow
+# kill process killmenow
+
 exec { 'pkill':
-  command => 'pkill -f killmenow',
-  path    => '/usr/bin',
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
